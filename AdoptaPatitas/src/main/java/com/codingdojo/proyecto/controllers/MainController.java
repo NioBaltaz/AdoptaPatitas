7328b644f8943d4327fa5c138e3324a0500dddde
Izu7 <emersonxc97@gmail.com>
2023-01-22 00:31:52 -0300

    123123

--- AdoptaPatitas/src/main/java/com/codingdojo/proyecto/controllers/MainController.java
+++ AdoptaPatitas/src/main/java/com/codingdojo/proyecto/controllers/MainController.java
@@ -1,5 +1,13 @@
 package com.codingdojo.proyecto.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
 
+	@GetMapping("/")
+	public String index() {
+		return "index.jsp";
+	}
 }
