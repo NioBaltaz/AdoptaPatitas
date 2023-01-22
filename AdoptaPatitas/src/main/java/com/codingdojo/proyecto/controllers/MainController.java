e6c89da8efb84afd428a4dd6dfaf481438cb533c
poraswan <paulacristinaolivarescortes@gmail.com>
2023-01-22 00:36:43 -0300

    pau

--- AdoptaPatitas/src/main/java/com/codingdojo/proyecto/controllers/MainController.java
+++ AdoptaPatitas/src/main/java/com/codingdojo/proyecto/controllers/MainController.java
@@ -1,5 +1,15 @@
 package com.codingdojo.proyecto.controllers;
 
-public class MainController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@Controller
+public class MainController {
+ 
+	@RequestMapping("/")
+	public String index() {
+		return "index.jsp";
+	}
+	
 }
